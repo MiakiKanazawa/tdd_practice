6d948eb83a48786718c65386f94dc9d72837df14
kanazawa <miaki.kanazawa@jrits.ricoh.co.jp>
2018-04-05 13:58:58 +0900

    3章p22上部
    - equalメソッドの実装

--- app/src/test/java/com/example/miakikanazawa/money/MoneyTest.java
+++ app/src/test/java/com/example/miakikanazawa/money/MoneyTest.java
@@ -3,6 +3,7 @@ package com.example.miakikanazawa.money;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -22,5 +23,6 @@ public class MoneyTest {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
