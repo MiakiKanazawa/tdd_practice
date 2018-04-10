987873ad10c618f05d6ab22321bb3a51e44df27d
kanazawa <miaki.kanazawa@jrits.ricoh.co.jp>
2018-04-10 16:22:05 +0900

    8章p49中央部
    - dollarメソッドの実装
    - サブクラスへの参照を減らした

--- app/src/test/java/com/example/miakikanazawa/money/MoneyTest.java
+++ app/src/test/java/com/example/miakikanazawa/money/MoneyTest.java
@@ -12,8 +12,7 @@ import static org.junit.Assert.assertTrue;
 public class MoneyTest {
     @Test
     public void testMultiplication() {
-        Dollar five = Money.dollar(5);
-        five.times(2);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
