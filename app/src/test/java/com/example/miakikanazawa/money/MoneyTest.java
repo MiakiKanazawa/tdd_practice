e578bc40d8ad09bf6f9c26f933b59d2e701f3a99
kanazawa <miaki.kanazawa@jrits.ricoh.co.jp>
2018-04-10 14:32:32 +0900

    8章p48下部
    - サブクラスへの参照を減らした

--- app/src/test/java/com/example/miakikanazawa/money/MoneyTest.java
+++ app/src/test/java/com/example/miakikanazawa/money/MoneyTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertTrue;
 public class MoneyTest {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
         five.times(2);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
